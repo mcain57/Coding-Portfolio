edcf18a6ccc483e4b4b1f1fbdfec2acca415a949
mcain57 <mc2607@cam.ac.uk>
2026-08-24 19:19:38 +0100

    installing virtual environment requirements

---
+++ pricing.py
@@ -0,0 +1,11 @@
+from scipy.stats import norm
+import numpy as np
+
+print(norm.cdf(0))
+
+def bs_call_price(S0, K, r, sigma, T):
+    print()
+
+def bs_put_price(S0, K, r, sigma, T):
+    print()
+
